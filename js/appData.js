e70299a1d4a28336f197d91fe1cdae2f7fa643ce
arun-art06 <arun.art06@gmail.com>
2014-01-24 14:11:55 +0400

    added pages

--- js/appData.js
+++ js/appData.js
@@ -2,23 +2,23 @@
       {
           "name": "veg-biriyani",
           "amount": "25 AED",
-          "photo": "images/best-food.png"
+          "photo": "css/images/best-food.png"
       }, {
           "name": "Mutton biriyani",
           "amount": "25 AED",
-          "photo": "images/biriyani1.png"
+          "photo": "css/images/biriyani1.png"
       }, {
           "name": "Chicken biriyani",
           "amount": "25 AED",
-          "photo": "images/biriyani2.png"
+          "photo": "css/images/biriyani2.png"
       }, {
           "name": "Doosai",
           "amount": "7 AED",
-          "photo": "images/food-dosai.png"
+          "photo": "css/images/food-dosai.png"
       }, {
           "name": "Idly",
           "amount": "5 AED / set (2pcs)",
-          "photo": "images/food-idli.png"
+          "photo": "css/images/food-idli.png"
       }
 ];
 
@@ -26,31 +26,31 @@ var drinks = [
      {
          "name": "Tomoto Juice",
          "amount": "3 AED",
-         "photo": "images/drink-tomato.png"
+         "photo": "css/images/drink-tomato.png"
      }, {
          "name": "Pine Apple Juice",
          "amount": "15 AED",
-         "photo": "images/drink-pineapple.png"
+         "photo": "css/images/drink-pineapple.png"
      }, {
          "name": "Pine Apple Juice",
          "amount": "15 AED",
-         "photo": "images/drink-apple.png"
+         "photo": "css/images/drink-apple.png"
      }, {
          "name": "Carrot Juice",
          "amount": "7 AED",
-         "photo": "images/drink-carrot.png"
+         "photo": "css/images/drink-carrot.png"
      }, {
          "name": "Orange Juice",
          "amount": "5 AED",
-         "photo": "images/drink-orange.png"
+         "photo": "css/images/drink-orange.png"
      }
 ];
 
 
 var menu = [
-    { page: 'views/mainItemsView.html', title: "Today special", description: "Wide range of special items like chicken,fish,mutton and forest food.", url: "images/best-food.png" },
-    { page: 'views/mainItemsView.html', title: "Fresh fruit juice", description: "specializes in mixing cold drinks to quench your thirst", url: "images/best-fruit.png" },
-    { page: 'views/mainItemsView.html', title: "Milk shakes", description: "All varity of mile shakes available", url: "images/rewards.png" },
-    { page: 'views/mainItemsView.html', title: "Soups", description: "all our soups tastes like mom made it", url: "images/best-soup.png" },
-    { page: 'views/mainItemsView.html', title: "Hot and Soft drinks", description: "Find and enjoy our, hot and cold drinks anytime", url: "images/love-friend.png" }
+    { page: '#special', title: "Today special", description: "Wide range of special items like chicken,fish,mutton and forest food.", url: "css/images/best-food.png" },
+    { page: '#special', title: "Fresh fruit juice", description: "specializes in mixing cold drinks to quench your thirst", url: "css/images/best-fruit.png" },
+    { page: '#special', title: "Milk shakes", description: "All varity of mile shakes available", url: "css/images/rewards.png" },
+    { page: '#special', title: "Soups", description: "all our soups tastes like mom made it", url: "css/images/best-soup.png" },
+    { page: '#special', title: "Hot and Soft drinks", description: "Find and enjoy our, hot and cold drinks anytime", url: "css/images/love-friend.png" }
 ];
