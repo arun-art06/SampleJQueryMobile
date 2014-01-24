443ded12a64da536be8f1373d81585b6dbfbda34
arun-art06 <arun.art06@gmail.com>
2014-01-24 12:47:56 +0400

    sample frame

---
+++ js/appData.js
@@ -0,0 +1,56 @@
+﻿var biriyani = [
+      {
+          "name": "veg-biriyani",
+          "amount": "25 AED",
+          "photo": "images/best-food.png"
+      }, {
+          "name": "Mutton biriyani",
+          "amount": "25 AED",
+          "photo": "images/biriyani1.png"
+      }, {
+          "name": "Chicken biriyani",
+          "amount": "25 AED",
+          "photo": "images/biriyani2.png"
+      }, {
+          "name": "Doosai",
+          "amount": "7 AED",
+          "photo": "images/food-dosai.png"
+      }, {
+          "name": "Idly",
+          "amount": "5 AED / set (2pcs)",
+          "photo": "images/food-idli.png"
+      }
+];
+
+var drinks = [
+     {
+         "name": "Tomoto Juice",
+         "amount": "3 AED",
+         "photo": "images/drink-tomato.png"
+     }, {
+         "name": "Pine Apple Juice",
+         "amount": "15 AED",
+         "photo": "images/drink-pineapple.png"
+     }, {
+         "name": "Pine Apple Juice",
+         "amount": "15 AED",
+         "photo": "images/drink-apple.png"
+     }, {
+         "name": "Carrot Juice",
+         "amount": "7 AED",
+         "photo": "images/drink-carrot.png"
+     }, {
+         "name": "Orange Juice",
+         "amount": "5 AED",
+         "photo": "images/drink-orange.png"
+     }
+];
+
+
+var menu = [
+    { page: 'views/mainItemsView.html', title: "Today special", description: "Wide range of special items like chicken,fish,mutton and forest food.", url: "images/best-food.png" },
+    { page: 'views/mainItemsView.html', title: "Fresh fruit juice", description: "specializes in mixing cold drinks to quench your thirst", url: "images/best-fruit.png" },
+    { page: 'views/mainItemsView.html', title: "Milk shakes", description: "All varity of mile shakes available", url: "images/rewards.png" },
+    { page: 'views/mainItemsView.html', title: "Soups", description: "all our soups tastes like mom made it", url: "images/best-soup.png" },
+    { page: 'views/mainItemsView.html', title: "Hot and Soft drinks", description: "Find and enjoy our, hot and cold drinks anytime", url: "images/love-friend.png" }
+];
